b1dd64c847a3d751f2a48b41a69c08e468b26c06
fwenchino <enriquecolladof@gmail.com>
2020-09-04 11:41:35 -0500

    updating files

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,35 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = {}
 
-    return None
+    # checking if value in caché
+    if limit in cache:
+        return cache[limit]
+
+    else:
+        # Calculate the entry to the caché
+        entry = None
+
+        for i1, w1 in enumerate(weights):
+            w2 = limit - w1
+
+            if w2 in weights:
+                if w2 != w1:
+                    i2 = weights.index(w2)
+                else:
+                    weights[i1] = w1 + 1
+                    i2 = weights.index(w2)
+
+                if i1 > i2:
+                    entry = (i1, i2)
+                    break
+                else:
+                    entry = (i2, i1)
+                    break
+
+        cache[limit] = entry
+
+    return entry
+
+print(get_indices_of_item_weights([4, 4], 2, 8))
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,38 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    '''
+    tackes in class object of ticket, and lentht of tickets
+    '''
+    cache = {}
 
-    return route
+    # Build the cache
+    for ticket in tickets:
+        if ticket.source is None:
+            cache['NONE'] = ticket.destination
+        if ticket.destination is None:
+            cache[ticket.source] = 'NONE'
+
+        cache[ticket.source] = ticket.destination
+
+    # Build the ordered list of tickets
+    ticket_list = []
+    switch = True
+    source = 'NONE'
+    while switch is True:
+        dest = cache[source]
+        source = dest
+        ticket_list.append(dest)
+        if dest == 'NONE':
+            switch = False
+
+    return ticket_list
+
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+reconstruct_trip(tickets, 3)
